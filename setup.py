2aae342ccc436a99d69697cc818205789b612a02
Clemens Brunner <clemens.brunner@gmail.com>
2015-08-13 15:02:03 +0200

    Files for uploading to PyPI.

--- setup.py
+++ setup.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
 
 
-versionfile = open('VERSION', 'r')
-ver = versionfile.read().strip()
-versionfile.close()
+with open('VERSION', encoding='utf-8') as version:
+    ver = version.read().strip()
 
-
-setup(name='scot-data',
-      version=ver,
-      description='Example data for the Source Connectivity Toolbox',
-      author='Martin Billinger',
-      author_email='martin.billinger@tugraz.at',
-      url='https://github.com/SCoT-dev/scot-data',
-      packages=['scotdata'],
-      package_data={'scotdata': ['*.mat']},
-      install_requires=['scot', 'numpy'])
+setup(
+    name='scot-data',
+    version=ver,
+    description='Example data for the Source Connectivity Toolbox',
+    url='https://github.com/scot-dev/scot-data',
+    author='SCoT Development Team',
+    author_email='scotdev@googlegroups.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='source connectivity EEG example data',
+    packages=['scotdata'],
+    package_data={'scotdata': ['*.mat']},
+    install_requires=['scot', 'numpy']
+)
